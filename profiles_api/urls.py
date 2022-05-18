db6163fccf3f7ad0d5133978fee2ec5dda1e4ea2
farghorbani <50452602+FarzanehGhorbani@users.noreply.github.com>
2022-05-18 18:41:49 +0430

    Added user profile feed api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,9 +4,12 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register("hello-viewset", views.HelloViewSet, basename="hello-viewset")
-router.register('profile', views.UserProfileViewSet) #UserProfileViewSet has query set then we dont need to basename
-
+router.register(
+    "profile", views.UserProfileViewSet
+)  # UserProfileViewSet has query set then we dont need to basename
+router.register("feed", views.ProfileFeedItemViewSet)
 urlpatterns = [
     path("hello-view/", views.HelloAPIView.as_view()),
+    path("login/", views.UserLoginApiView.as_view()),
     path("", include(router.urls)),
 ]
